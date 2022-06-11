8708490a69f3954464489648ca684736eb67b8f7
Harsh Keshri <harshkeshri1234567@gmail.com>
2022-06-11 20:53:52 +0530

    Warning Removed

--- src/assests/JavaScript/SearchComponents/Search.js
+++ src/assests/JavaScript/SearchComponents/Search.js
@@ -1,11 +1,9 @@
-import { React, useState } from "react";
+import { React} from "react";
 
 const Search = (props) => {
-  const [input, setInput] = useState("");
 
   const inputChanged = (event) => {
-    setInput(event.target.value);
-    props.onSearched(input);
+    props.onSearched(event.target.value);
   };
 
   // const inputChangedBt = () => {
@@ -17,7 +15,6 @@ const Search = (props) => {
       <input
         id="search-input"
         placeholder="Search Here"
-        value={input}
         onChange={inputChanged}
       />
       <button onClick={inputChanged} id="search-button">
